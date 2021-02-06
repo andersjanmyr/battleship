e1c16b6b0bba545c7155bead5a5f0ebe1420c257
Anders Janmyr <anders@janmyr.com>
2021-02-06 06:41:05 +0100

    Clean up

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 
 def create_board(width, height):
-    board =  [ [ '-' for i in range(width) ] for j in range(height) ]
+    board =  [['-' for i in range(width)] for j in range(height)]
     return board
 
 def to_string(board):
-    return "   %(header)s\n%(rows)s\n" % \
-        { "header": top_header(10),
-         "rows": rows_with_num(board) }
+    return "   %s\n%s\n" % (top_header(10), rows_with_num(board))
 
 def top_header(width):
     start = ord('A')
-    header = [ chr(i) for i in range(start, start + width) ]
+    header = [chr(i) for i in range(start, start + width)]
     return " ".join(header)
 
 def rows_with_num(board):
-     return "\n".join([ row_with_num(i, row) for i, row in enumerate(board) ])
+     return "\n".join([row_with_num(i, row) for i, row in enumerate(board)])
 
 def row_with_num(i, row):
-    return "%(i)2d %(row)s" % { "i": i+1, "row": " ".join(row) }
+    return "%2d %s" % (i+1, " ".join(row))
 
 def print_board(board):
     print(to_string(board))
