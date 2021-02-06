0491e7bd6d32bc3a534b793f573089bdf06b84d4
Anders Janmyr <anders@janmyr.com>
2021-02-06 13:02:18 +0100

    Extracted Board Class

--- main.py
+++ main.py
@@ -1,29 +1,80 @@
 import random
+import sys
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [['~' for i in range(width)] for j in range(height)]
+
+    def __str__(self):
+        return "   %s\n%s\n" % (self.top_header(), self.rows_with_num())
+
+    def top_header(self):
+        start = ord('A')
+        header = [chr(i) for i in range(start, start + self.width)]
+        return " ".join(header)
+
+    def rows_with_num(self):
+         return "\n".join([self.row_with_num(i, row) for i, row in enumerate(self.board)])
+
+    def row_with_num(self, i, row):
+        return "%2d %s" % (i+1, " ".join(row))
+
+    def get(self, coord):
+        (x, y) = coord
+        row = self.board[y]
+        return row[x]
+
+    def set(self, coord, i):
+        (x, y) = coord
+        self.board[y][x] = i
+
+    def in_bounds(self, coord):
+        (x, y) = coord
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def match_count(self, c):
+        is_match = lambda i: i == c
+        matches = filter(is_match, [i for row in self.board for i in row])
+        return len(list(matches))
+
+
+X_INDEX="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def main():
-    board_with_ships = create_board(10, 10)
-    place_ships(board_with_ships, [4, 3, 3, 2, 2, 2, 1, 1, 1, 1])
-    game_board = create_board(10, 10)
-    game_loop(board_with_ships, game_board)
+    try:
+        (width, height, ships) = parse_args(sys.argv)
+    except ValueError as err:
+        print(err)
+        usage()
+        exit(1)
 
-def create_board(width, height):
-    board =  [['-' for i in range(width)] for j in range(height)]
-    return board
+    solution_board = Board(width, height)
+    place_ships(solution_board, ships)
+    print(solution_board)
+    game_board = Board(width, height)
+    game_loop(solution_board, game_board)
 
-def board_to_string(board):
-    width = len(board[0])
-    return "   %s\n%s\n" % (top_header(width), rows_with_num(board))
+def parse_args(args):
+    width = 10 if len(args) < 2 else int(args[1])
+    height = 10 if len(args) < 3 else int(args[2])
+    if width > len(X_INDEX):
+        raise ValueError("Max width is: %d" % len(X_INDEX))
+    if height > 50:
+        raise ValueError("Max height is: %d" % 50)
+    if len(sys.argv) < 4:
+        ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+    else:
+        try:
+            ships = list(map(lambda s: int(s), args[3:]))
+        except:
+            raise ValueError("Ships must be numeric values: '%s'" % " ".join(args[3:]))
+    return (width, height, ships)
 
-def top_header(width):
-    start = ord('A')
-    header = [chr(i) for i in range(start, start + width)]
-    return " ".join(header)
-
-def rows_with_num(board):
-     return "\n".join([row_with_num(i, row) for i, row in enumerate(board)])
-
-def row_with_num(i, row):
-    return "%2d %s" % (i+1, " ".join(row))
+def usage():
+    print("Usage: python main.py [width] [height] [ships]")
+    print("Example: python main.py 15 10 7 6 5 1 1 1")
 
 def place_ships(board, ships):
     for ship in ships:
@@ -36,8 +87,8 @@ def place_ship(board, ship):
     update_board(board, coords)
 
 def random_coords(board, ship):
-    h = len(board)
-    w = len(board[0])
+    h = board.height
+    w = board.width
     dir = random.randint(0, 1)
     if dir == 0:
         w -= ship
@@ -59,60 +110,53 @@ def valid_coords(board, coords):
 def valid_coord(board, coord):
     (x, y) = coord
     coords = [(x-1,y), (x,y), (x+1,y), (x,y-1), (x,y+1)]
-    in_bounds = lambda c: on_board(board, c)
+    in_bounds = lambda c: board.in_bounds(c)
     possible = filter(in_bounds, coords)
-    invalid = filter(lambda c: board[c[1]][c[0]] == ':', possible)
+    invalid = filter(lambda c: board.get(c) == ':', possible)
     return len(list(invalid)) == 0
 
-def on_board(board, coord):
-    (x, y) = coord
-    return x >= 0 and x < len(board[0]) and y >=0 and y < len(board)
-
 def update_board(board, coords):
-    for (x, y) in coords:
-        board[y][x] = ':'
+    for c in coords:
+        board.set(c, ':')
 
-def game_loop(board_with_ships, game_board):
+def game_loop(solution_board, game_board):
     guesses = 0
-    ship_parts = count_in_board(board_with_ships, ':')
+    ship_parts = solution_board.match_count(':')
     while not game_over(game_board, ship_parts):
         print("Guesses: %d" % guesses)
-        print(board_to_string(game_board))
-        pos = wait_for_input()
+        print(game_board)
+        pos = wait_for_input(game_board.width, game_board.height)
         guesses += 1
-        (x, y) = pos_to_coord(pos)
-        if board_with_ships[y][x] == ':':
-            game_board[y][x] = 'x'
+        c = pos_to_coord(pos)
+        if solution_board.get(c) == ':':
+            game_board.set(c, 'x')
         else:
-            game_board[y][x] = 'o'
-
-def count_in_board(board, c):
-    is_match = lambda i: i == c
-    hits = filter(is_match, [i for row in board for i in row])
-    return len(list(hits))
+            game_board.set(c, 'o')
 
 def game_over(game_board, ship_parts):
-    return count_in_board(game_board, 'x') == ship_parts
+    return game_board.match_count('x') == ship_parts
 
 
-def wait_for_input():
+def wait_for_input(width, height):
     val = input("Guess: ")
-    while not input_valid(val):
-        print("Invalid input '%s', valid format: A1-J10" % val)
+    while not input_valid(val, width, height):
+        max_x = X_INDEX[width-1]
+        max_y = height
+        print("Invalid input '%s', valid format: A1-%s%d" % (val, max_x, max_y))
         val = input("Guess: ")
     return val
 
-def input_valid(val):
+def input_valid(val, width, height):
     if len(val) < 2:
         return False
-    if not val[0].upper() in "ABCDEFGHIJ":
+    if not val[0].upper() in X_INDEX[:width]:
         return False
     i = int(val[1:])
-    return i >= 1 and i <= 10
+    return i >= 1 and i <= height
 
 
 def pos_to_coord(pos):
-    x = "ABCDEFGHIJ".index(pos[0].upper())
+    x = X_INDEX.index(pos[0].upper())
     y = int(pos[1:])-1
     return (x, y)
 
